9b4cff814a66dd47eef22d1020beae0222abb1a3
Millad Ayoubi <Milladayoubi@gmail.com>
2023-07-19 02:51:18 -0400

    Search Excercises

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -53,7 +53,7 @@ const SearchExercises = ({ setExercises, bodyPart, setBodyPart }) => {
           placeholder="Search Exercises"
           type="text"
         />
-        <Button className="search-btn" sx={{ bgcolor: '#FF2625', color: '#fff', textTransform: 'none', width: { lg: '173px', xs: '80px' }, height: '56px', position: 'absolute', right: '0px', fontSize: { lg: '20px', xs: '14px' } }} onClick={handleSearch}>
+        <Button className="search-btn" sx={{ bgcolor: '#FF2625', borderRadius: '100px',  color: '#fff', textTransform: 'none', width: { lg: '173px', xs: '80px' }, height: '56px', position: 'absolute', right: '0px', fontSize: { lg: '20px', xs: '14px' } }} onClick={handleSearch}>
           Search
         </Button>
       </Box>
