9f97bbe8caaf99c4e62cef22fdd66502d24cd1a0
Millad Ayoubi <Milladayoubi@gmail.com>
2023-07-19 03:34:51 -0400

    Card Design

--- src/components/BodyPart.js
+++ src/components/BodyPart.js
@@ -8,13 +8,12 @@ const BodyPart = ({ item, setBodyPart, bodyPart }) => (
     alignItems="center"
     justifyContent="center"
     className="bodyPart-card"
-    sx={bodyPart === item ? { border: '4px solid #FF2625', background: '#fff', borderRadius: '20px', width: '270px', height: '282px', cursor: 'pointer', gap: '47px' } : { background: '#fff', borderRadius: '20px', width: '270px', height: '282px', cursor: 'pointer', gap: '47px' }}
+    sx={bodyPart === item ? {  border: '2px solid #FFFFFF', background: '#131338', borderRadius: '20px', width: '200px', height: '62px', cursor: 'pointer', gap: '47px' } : { background: '#131338', borderRadius: '20px', width: '200px', height: '62px', cursor: 'pointer', gap: '47px' }}
     onClick={() => {
       setBodyPart(item);
     }}
   >
-    <img src={Icon} alt="dumbbell" style={{ width: '40px', height: '40px' }} />
-    <Typography className="text-black text-lg font-bold" fontWeight="bold" sx={{ fontSize: { lg: '24px', xs: '20px' } }} >{item}</Typography>
+    <Typography className="text-white" fontWeight="semibold" sx={{ fontSize: { lg: '20px', xs: '20px' } }} >{item}</Typography>
   </Stack>
 );
 
--- src/components/Excercises.js
+++ src/components/Excercises.js
@@ -40,7 +40,7 @@ const Exercises = ({ exercises, setExercises, bodyPart }) => {
   if (!currentExercises.length) return <Loader />;
 
   return (
-    <Box id="exercises" sx={{ mt: { lg: '109px' } }} mt="50px" p="20px">
+    <Box id="exercises" sx={{ mt: { lg: '40px' } }} mt="50px" p="20px">
       <Typography variant="h4" fontWeight="bold" sx={{ fontSize: { lg: '44px', xs: '30px' } }} mb="46px"></Typography>
       <Stack direction="row" sx={{ gap: { lg: '107px', xs: '50px' } }} flexWrap="wrap" justifyContent="center">
         {currentExercises.map((exercise, idx) => (
